441b088a9f54fd36f4b55fcca80c337ce5e777d3
Jordan Dennison <jpd0021@mix.wvu.edu>
2024-11-01 15:57:17 -0400

    Changes added

--- phase2/Parser.java
+++ phase2/Parser.java
@@ -16,10 +16,9 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.ArrayList;
-import java.util.Iterator;
 
 public class Parser {
 
@@ -33,13 +32,18 @@ public class Parser {
 			this.data = data;
 		}
 
+		@Override
 		public String toString() {
 			return tokenIdentifier + " Data:  " + data;
 		}
 	}
 
 	Queue<Token> tokens;
-	Queue<Queue<String>> atoms = new LinkedList<Queue<String>>();
+	static Queue<String> atoms = new LinkedList<String>();
+	static int LHS = 0;
+	static int RHS = 0;
+	int destinationLabel = 1;
+
 	private Token currentToken;
 	int index = 0;
 
@@ -55,11 +59,12 @@ public class Parser {
 
 		// Get the first token
 		currentToken = tokens.poll();
-
 		// Start parsing the expression
 		parseStatement();
 
 	}
+
+	int tempValue = 0;
 
 	private void parseDecl() {
 
@@ -67,48 +72,58 @@ public class Parser {
 
 		advance();
 
-		match("IDENTIFIER");
+		String Ident = match("IDENTIFIER").data;
+		tempValue++;
+		String tempVar = "temp" + tempValue;
+
+		String atomString = "(MOV," + tempVar + ",," + Ident + ")";
+		atoms.add(atomString);
 
 		advance();
 
-		match("COLON");
+		if (currentToken.tokenIdentifier.equals("COLON")) {
+
+			match("COLON");
+
+			advance();
+
+			// TODO: NOT INCLUDED IN DOC
+			if (currentToken.tokenIdentifier.equals("MUT_KEYWORD")) {
+
+				System.out.println("Matched: " + currentToken.tokenIdentifier + " --> " + currentToken.data);
+
+				advance();
+
+				if (currentToken.tokenIdentifier.equals("I32_KEYWORD")
+						|| currentToken.tokenIdentifier.equals("I64_KEYWORD")
+						|| currentToken.tokenIdentifier.equals("F32_KEYWORD")
+						|| currentToken.tokenIdentifier.equals("F64_KEYWORD")) {
+
+					System.out.println("Matched: " + currentToken.tokenIdentifier + " --> " + currentToken.data);
+
+					advance();
+
+				} else {
+					throw new RuntimeException("Error: Expected type keyword but got " + currentToken.tokenIdentifier);
+				}
+
+			}
+
+		}
+
+		match("ASSIGNMENT_OPERATOR");
 
 		advance();
 
-		// TODO: NOT INCLUDED IN DOC
-		if (currentToken.tokenIdentifier.equals("MUT_KEYWORD")) {
+		String yep = parseExpr();
 
-			System.out.println("Matched: " + currentToken.tokenIdentifier + " --> " + currentToken.data);
+		System.out.println("YEP IS " + yep);
 
-			advance();
-
-		}
-
-		if (currentToken.tokenIdentifier.equals("I32_KEYWORD") || currentToken.tokenIdentifier.equals("I64_KEYWORD")
-				|| currentToken.tokenIdentifier.equals("F32_KEYWORD")
-				|| currentToken.tokenIdentifier.equals("F64_KEYWORD")) {
-
-			System.out.println("Matched: " + currentToken.tokenIdentifier + " --> " + currentToken.data);
-
-			advance();
-
-			match("ASSIGNMENT_OPERATOR");
-
-			advance();
-
-			parseExpr();
-
-			match("SEMICOLON");
-
-		} else {
-			throw new RuntimeException("Error: Expected type keyword but got " + currentToken.tokenIdentifier);
-		}
+		match("SEMICOLON");
 
 	}
 
 	public void parseAssign() {
-
-		Queue<String> atom = new LinkedList<String>();
 
 		match("IDENTIFIER");
 
@@ -124,18 +139,19 @@ public class Parser {
 
 		advance();
 
-		atoms.add(atom);
-
 	}
 
-	public void parseBase() {
+	public String parseBase() {
+
+		LHS++;
 
 		if (currentToken.tokenIdentifier.equals("INT_LITERAL") || currentToken.tokenIdentifier.equals("FLOAT_LITERAL")
 				|| currentToken.tokenIdentifier.equals("IDENTIFIER")) {
 
 			System.out.println("Matched: " + currentToken.tokenIdentifier + " --> " + currentToken.data);
-
+			String toRet = currentToken.data;
 			advance();
+			return toRet;
 
 		} else if (currentToken.tokenIdentifier.equals("LEFT_PARANTHESIS")) {
 
@@ -143,9 +159,11 @@ public class Parser {
 
 			advance();
 
-			parseExpr();
+			String Result = parseExpr();
 
 			match("RIGHT_PARANTHESIS");
+			advance();
+			return Result;
 
 		}
 
@@ -158,53 +176,113 @@ public class Parser {
 
 	}
 
-	public void parseExpr() {
+	// public void parseExpr() {
+	// tempValue++;
 
-		parseMulDiv();
+	// System.out.println("IN PARSEEXPR");
 
-		parseExprTail();
+	// parseMulDiv();
 
-	}
+	// parseExprTail();
 
-	public void parseMulDiv() {
+	// }
 
-		parseBase();
+	// public void parseMulDiv() {
+	// System.out.println("IN PARSEMULDIV");
+	// parseBase();
 
-		parseMulDivTail();
+	// parseMulDivTail();
 
-	}
+	// }
 
-	public void parseMulDivTail() {
+	// public void parseMulDivTail() {
+	// System.out.println("IN PARSEMULDIVTAIL");
 
-		if (currentToken.tokenIdentifier.equals("MULTIPLICATION_OPERATOR")
-				|| currentToken.tokenIdentifier.equals("DIVISION_OPERATOR")) {
+	// if (currentToken.tokenIdentifier.equals("MULTIPLICATION_OPERATOR")
+	// || currentToken.tokenIdentifier.equals("DIVISION_OPERATOR")) {
 
-			System.out.println("Matched: " + currentToken.tokenIdentifier + " --> " + currentToken.data);
+	// System.out.println("Matched: " + currentToken.tokenIdentifier + " --> " +
+	// currentToken.data);
+	// RHS++;
+	// advance();
 
-			advance();
+	// parseBase();
 
-			parseBase();
+	// parseMulDivTail();
 
-			parseMulDivTail();
+	// }
 
-		}
+	// }
 
-	}
+	// public void parseExprTail() {
+	// System.out.println("IN PARSEEXPRTAIL");
+	// if (currentToken.tokenIdentifier.equals("ADDITION_OPERATOR")
+	// || currentToken.tokenIdentifier.equals("SUBTRACTION_OPERATOR")) {
 
-	public void parseExprTail() {
+	// System.out.println("Matched: " + currentToken.tokenIdentifier + " --> " +
+	// currentToken.data);
+	// RHS++;
+	// advance();
+
+	// parseMulDiv();
+
+	// parseExprTail();
+
+	// }
+
+	// }
+
+	public String parseExpr() {
+		tempValue++;
+
+		String yep = parseAddSub();
 
 		if (currentToken.tokenIdentifier.equals("ADDITION_OPERATOR")
 				|| currentToken.tokenIdentifier.equals("SUBTRACTION_OPERATOR")) {
 
 			System.out.println("Matched: " + currentToken.tokenIdentifier + " --> " + currentToken.data);
-
+			RHS++;
+			String operation = currentToken.data;
 			advance();
-
-			parseMulDiv();
-
-			parseExprTail();
+			String yep2 = parseExpr();
+			if (operation.equals("+")) {
+				final String toRet = new String("ADD(" + yep2 + "," + yep + ")");
+				return toRet;
+			} else {
+				String toRet = new String("SUB(" + yep2 + "," + yep + ")");
+				return toRet;
+			}
 
 		}
+
+		return yep;
+	}
+
+	public String parseAddSub() {
+
+		String yep = parseBase();
+
+		if (currentToken.tokenIdentifier.equals("MULTIPLICATION_OPERATOR")
+				|| currentToken.tokenIdentifier.equals("DIVISION_OPERATOR")) {
+
+			System.out.println("Matched: " + currentToken.tokenIdentifier + " --> " + currentToken.data);
+			RHS++;
+			String operation = currentToken.data;
+			advance();
+			String yep2 = parseBase();
+			if (operation.equals("*")) {
+				String toRet = "MULT(" + yep2 + "," + yep + ")";
+
+				return toRet;
+			} else {
+				String toRet = "DIV(" + yep2 + "," + yep + ")";
+
+				return toRet;
+			}
+
+		}
+
+		return yep;
 
 	}
 
@@ -221,21 +299,31 @@ public class Parser {
 
 			parseAssign();
 
+			advance();
+
 		} else if (currentToken.tokenIdentifier.equals("LET_KEYWORD")) {
 
 			parseDecl();
+
+			advance();
 
 		} else if (currentToken.tokenIdentifier.equals("FOR_KEYWORD")) {
 
 			parseFor();
 
+			advance();
+
 		} else if (currentToken.tokenIdentifier.equals("WHILE_KEYWORD")) {
 
 			parseWhile();
 
+			advance();
+
 		} else {
 
 			parseStatements();
+
+			advance();
 
 		}
 	}
@@ -243,7 +331,13 @@ public class Parser {
 	public void parseStatements() {
 
 		// TODO: ADD IF FOR START OF ANY PRODUCTION
-		parseStatement();
+		if (currentToken.tokenIdentifier.equals("LET_KEYWORD") || currentToken.tokenIdentifier.equals("IF_KEYWORD")
+				|| currentToken.tokenIdentifier.equals("IDENTIFIER")
+				|| currentToken.tokenIdentifier.equals("FOR_KEYWORD")
+				|| currentToken.tokenIdentifier.equals("WHILE_KEYWORD")
+				|| currentToken.tokenIdentifier.equals("LET_KEYWORD")) {
+			parseStatement();
+		}
 
 		// TODO: ELSE THROW ERROR
 
@@ -298,12 +392,20 @@ public class Parser {
 
 	}
 
-	public void parseCond() {
+	public int parseCond() {
+
+		String operator;
+		String leftOp;
+		String rightOp;
+		int destCount = ++destinationLabel;
+		String destLbl = "L" + destCount;
 
 		if (currentToken.tokenIdentifier.equals("IDENTIFIER") || currentToken.tokenIdentifier.equals("INT_LITERAL")
 				|| currentToken.tokenIdentifier.equals("FLOAT_LITERAL")) {
 
 			System.out.println("Matched: " + currentToken.tokenIdentifier + " --> " + currentToken.data);
+
+			leftOp = currentToken.data;
 
 			advance();
 
@@ -322,6 +424,20 @@ public class Parser {
 
 			System.out.println("Matched: " + currentToken.tokenIdentifier + " --> " + currentToken.data);
 
+			if (currentToken.tokenIdentifier.equals("IS_EQUAL_OPERATOR")) {
+				operator = "6";
+			} else if (currentToken.tokenIdentifier.equals("NOT_EQUAL_OPERATOR")) {
+				operator = "1";
+			} else if (currentToken.tokenIdentifier.equals("LESS_THAN_OPERATOR")) {
+				operator = "5";
+			} else if (currentToken.tokenIdentifier.equals("GREATER_THAN_OPERATOR")) {
+				operator = "4";
+			} else if (currentToken.tokenIdentifier.equals("LESS_THAN_OR_EQUAL_OPERATOR")) {
+				operator = "3";
+			} else {
+				operator = "2";
+			}
+
 			advance();
 
 		} else {
@@ -335,6 +451,8 @@ public class Parser {
 
 			System.out.println("Matched: " + currentToken.tokenIdentifier + " --> " + currentToken.data);
 
+			rightOp = currentToken.data;
+
 			advance();
 
 		} else {
@@ -343,10 +461,22 @@ public class Parser {
 
 		}
 
+		String atomString = "(TST, " + leftOp + "," + rightOp + ",," + operator + "," + destLbl + ")";
+		atoms.add(atomString);
+
+		return destCount;
+
 	}
 
-	// TODO: I don't understand RUST FOR loops
 	public void parseFor() {
+
+		String counterVar;
+		String start;
+		String end;
+		int destCount = ++destinationLabel;
+		String topLabel = "L" + destCount;
+		destCount = ++destinationLabel;
+		String endLabel = "L" + destCount;
 
 		match("FOR_KEYWORD");
 
@@ -358,13 +488,28 @@ public class Parser {
 
 		match("IDENTIFIER");
 
+		counterVar = currentToken.data;
+
 		advance();
 
 		match("IN_KEYWORD");
 
 		advance();
 
-		match("IDENTIFIER");
+		// TODO: Syntax is wierd
+		match("INT_LITERAL");
+
+		start = currentToken.data;
+
+		advance();
+
+		match("INT_LITERAL");
+
+		end = currentToken.data;
+
+		advance();
+
+		match("RIGHT_PARANTHESIS");
 
 		advance();
 
@@ -372,13 +517,37 @@ public class Parser {
 
 		advance();
 
+		String atomString = "(MOV, " + start + ",,,," + counterVar + ")";
+		atoms.add(atomString);
+
+		atomString = "(LBL,,,,," + topLabel + ")";
+		atoms.add(atomString);
+
+		atomString = "(TST, " + counterVar + ", " + end + ",, " + 5 + ", " + endLabel + ")";
+		atoms.add(atomString);
+
 		parseStatement();
 
+		atomString = "(ADD, " + counterVar + ", " + 1 + ", " + counterVar + ")";
+		atoms.add(atomString);
+
+		atomString = "(JMP,,,,," + topLabel + ")";
+		atoms.add(atomString);
+
+		atomString = "(LBL,,,,," + endLabel + ")";
+		atoms.add(atomString);
+
 		match("RIGHT_CURLY_BRACKET");
+
+		advance();
 
 	}
 
 	public void parseWhile() {
+
+		String counterVar;
+		int destCount = ++destinationLabel;
+		String topLabel = "L" + destCount;
 
 		match("WHILE_KEYWORD");
 
@@ -388,7 +557,12 @@ public class Parser {
 
 		advance();
 
-		parseCond();
+		counterVar = currentToken.data;
+
+		String atomString = "(LBL,,,,," + topLabel + ")";
+		atoms.add(atomString);
+
+		int returnLbl = parseCond();
 
 		match("RIGHT_PARANTHESIS");
 
@@ -400,20 +574,28 @@ public class Parser {
 
 		parseStatement();
 
+		String endLabel = "L" + returnLbl;
+		atomString = "(JMP,,,,," + topLabel + ")";
+		atoms.add(atomString);
+
+		atomString = "(LBL,,,,," + endLabel + ")";
+		atoms.add(atomString);
+
 		match("RIGHT_CURLY_BRACKET");
 
 	}
 
-	public void match(String expectedToken) {
+	public Token match(String expectedToken) {
 
 		if (currentToken.tokenIdentifier.equals(expectedToken)) {
 
 			System.out.println("Matched: " + expectedToken + " --> " + currentToken.data);
+			return currentToken;
 
 		} else {
 
 			System.out.println("Error: Expected " + expectedToken + " but got " + currentToken.tokenIdentifier);
-
+			return null;
 		}
 
 	}
@@ -461,14 +643,42 @@ public class Parser {
 
 		// Test for let statement
 		Queue<Token> letTest = new LinkedList<>();
+
+		letTest.add(new Token("WHILE_KEYWORD", "while"));
+		letTest.add(new Token("LEFT_PARANTHESIS", "("));
+		letTest.add(new Token("IDENTIFIER", "y"));
+		letTest.add(new Token("GREATER_THAN_OPERATOR", ">"));
+		letTest.add(new Token("INT_LITERAL", "5"));
+		letTest.add(new Token("RIGHT_PARANTHESIS", ")"));
+		letTest.add(new Token("LEFT_CURLY_BRACKET", "{"));
+
 		letTest.add(new Token("LET_KEYWORD", "let"));
 		letTest.add(new Token("IDENTIFIER", "id"));
-		letTest.add(new Token("COLON", ":"));
-		letTest.add(new Token("MUT_KEYWORD", "mut"));
-		letTest.add(new Token("I32_KEYWORD", "i32"));
+		// letTest.add(new Token("COLON", ":"));
+		// letTest.add(new Token("MUT_KEYWORD", "mut"));
+		// letTest.add(new Token("I32_KEYWORD", "i32"));
 		letTest.add(new Token("ASSIGNMENT_OPERATOR", "="));
+
+		letTest.add(new Token("IDENTIFIER", "y"));
+		letTest.add(new Token("MULTIPLICATION_OPERATOR", "*"));
+		letTest.add(new Token("LEFT_PARANTHESIS", "("));
 		letTest.add(new Token("IDENTIFIER", "x"));
+		letTest.add(new Token("ADDITION_OPERATOR", "+"));
+		letTest.add(new Token("IDENTIFIER", "y"));
+		letTest.add(new Token("DIVISION_OPERATOR", "/"));
+		letTest.add(new Token("IDENTIFIER", "z"));
+
+		letTest.add(new Token("ADDITION_OPERATOR", "+"));
+		letTest.add(new Token("IDENTIFIER", "i"));
+		letTest.add(new Token("RIGHT_PARANTHESIS", ")"));
 		letTest.add(new Token("SEMICOLON", ";"));
+
+		letTest.add(new Token("RIGHT_CURLY_BRACKET", "}"));
+
+		// "MULTIPLICATION_OPERATOR"
+		// "DIVISION_OPERATOR"
+		// "ADDITION_OPERATOR"
+		// "SUBTRACTION_OPERATOR"
 
 		System.out.println("");
 		System.out.println("//////////////////////////////////////////////////");
@@ -476,13 +686,18 @@ public class Parser {
 		System.out.println("//////////////////////////////////////////////////");
 		System.out.println("");
 
-		Parser parser = new Parser(tokensTest);
+		Parser parser = new Parser(letTest);
 		parser.parse();
 
 		System.out.println("");
 		System.out.println("//////////////////////////////////////////////////");
 		System.out.println("Parser Test didn't crash!");
 		System.out.println("//////////////////////////////////////////////////");
+
+		while (!atoms.isEmpty()) {
+			System.out.println(atoms.remove());
+		}
+		System.out.println("Vars: " + LHS + " Operations: " + RHS);
 	}
 
 }
