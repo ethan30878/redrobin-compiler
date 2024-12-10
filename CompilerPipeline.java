692b6f8b6b85512db3e5d0c49a9792674d1e5978
linuxx-lord <mjc00009@mix.wvu.edu>
2024-12-10 14:13:02 -0500

    Forgot how to code Java good

--- CompilerPipeline.java
+++ CompilerPipeline.java
@@ -2,9 +2,26 @@ import phase3.*;
 import phase2.*;
 import phase1.*;
 
-class Main() {
+import java.util.Scanner;
+import java.io.File;
+
+class CompilerPipeline {
+
 
 	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		while(true) {
+			System.out.println("Enter input file: ");
+			String inputFile = sc.nextLine();
+			File f = new File(inputFile);
+			if(f.exists() && !f.isDirectory()) {
+				break;
+			}else {
+				System.out.println("File does not exist. Please Try again: ");
+			}
 
+		}
+		System.out.println("Enter output file: ");
+		String outputFile = sc.nextLine();
 	}
 }
